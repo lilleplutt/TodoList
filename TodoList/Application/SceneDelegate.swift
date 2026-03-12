65cbb86d23d9b422b76d68db68be5ebdfe181fe1
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2026-03-12 11:08:30 +0300

    add TodoListRouter

--- TodoList/Application/SceneDelegate.swift
+++ TodoList/Application/SceneDelegate.swift
@@ -8,7 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: TodoListViewController())
+        window?.rootViewController = UINavigationController(rootViewController: TodoListRouter.createModule())
         window?.makeKeyAndVisible()
     }
 }
---
+++ TodoList/Modules/TodoList/Router/TodoListRouter.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+final class TodoListRouter {
+    
+    weak var view: UIViewController?
+    
+    static func createModule() -> UIViewController {
+        let view = TodoListViewController()
+        let presenter = TodoListPresenter()
+        let interactor = TodoListInteractor()
+        let router = TodoListRouter()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.output = presenter
+        router.view = view
+        
+        return view
+    }
+}
+
+// MARK: - TodoListRouterInput
+extension TodoListRouter: TodoListRouterInput {
+    
+    func showAddTodo() {
+        let detailVC = TodoDetailRouter.createModule(todo: nil)
+        detailVC.modalPresentationStyle = .pageSheet
+        view?.present(detailVC, animated: true)
+    }
+    
+    func showEditTodo(_ todo: Todo) {
+        let detailVC = TodoDetailRouter.createModule(todo: todo)
+        detailVC.modalPresentationStyle = .pageSheet
+        view?.present(detailVC, animated: true)
+    }
+}

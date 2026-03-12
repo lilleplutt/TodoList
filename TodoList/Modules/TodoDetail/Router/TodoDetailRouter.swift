9cc8c6679066ba89ec3f7152672baa8450965099
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2026-03-12 11:14:50 +0300

    add TodoDetailRouter

---
+++ TodoList/Modules/TodoDetail/Router/TodoDetailRouter.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+final class TodoDetailRouter {
+    
+    weak var view: UIViewController?
+    
+    func createModule() -> UIViewController {
+        let view = TodoDetailViewController()
+        let presenter = TodoDetailPresenter()
+        let interactor = TodoDetailInteractor()
+        let router = TodoDetailRouter()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.output = presenter
+        router.view = view
+        
+        return views
+    }
+}
+
+//MARK: - TodoDetailRouterInput
+extension TodoDetailRouter: TodoDetailRouterInput {
+    func dismiss() {
+        view?.dismiss(animated: true)
+    }
+}
+

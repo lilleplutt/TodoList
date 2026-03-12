9a12f47552e3c3666aa85f4115fe644861548481
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2026-03-12 11:26:38 +0300

    add todoListVC

--- TodoList/Modules/TodoList/View/TodoListViewController.swift
+++ TodoList/Modules/TodoList/View/TodoListViewController.swift
@@ -2,5 +2,145 @@ import UIKit
 
 final class TodoListViewController: UIViewController {
     
+    var presenter: TodoListViewOutput?
+    
+    // MARK: - UI Elements
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.separatorStyle = .none
+        table.backgroundColor = .systemBackground
+        return table
+    }()
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private let emptyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Нет задач"
+        label.textColor = .secondaryLabel
+        label.textAlignment = .center
+        label.isHidden = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var todos: [Todo] = []
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupNavigationBar()
+        setupSearchController()
+        presenter?.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presenter?.viewDidLoad()
+    }
+    
+    // MARK: - Setup
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        view.addSubview(emptyLabel)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(TodoCell.self, forCellReuseIdentifier: TodoCell.identifier)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            emptyLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emptyLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func setupNavigationBar() {
+        title = "Мои задачи"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(didTapAdd)
+        )
+    }
+    
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Поиск задач"
+        navigationItem.searchController = searchController
+    }
+    
+    // MARK: - Actions
+    @objc private func didTapAdd() {
+        presenter?.didTapAddTodo()
+    }
+}
+
+// MARK: - TodoListViewInput
+extension TodoListViewController: TodoListViewInput {
+    
+    func showTodos(_ todos: [Todo]) {
+        self.todos = todos
+        emptyLabel.isHidden = !todos.isEmpty
+        tableView.reloadData()
+    }
+    
+    func showError(_ message: String) {
+        let alert = UIAlertController(title: "Ошибка", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension TodoListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TodoCell.identifier, for: indexPath) as? TodoCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: todos[indexPath.row])
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension TodoListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        presenter?.didTapEdit(todos[indexPath.row])
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Удалить") { [weak self] _, _, completion in
+            guard let self else { return }
+            self.presenter?.didTapDelete(self.todos[indexPath.row])
+            completion(true)
+        }
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+}
+
+// MARK: - UISearchResultsUpdating
+extension TodoListViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let query = searchController.searchBar.text ?? ""
+        presenter?.didSearch(query)
+    }
 }
 

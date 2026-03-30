be19850d2bec789f9ab2fce15aead30b953cd06a
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2026-03-30 11:33:53 +0300

    add async/await in TodoListInteractor

--- TodoList/Modules/TodoList/Interactor/TodoListInteractor.swift
+++ TodoList/Modules/TodoList/Interactor/TodoListInteractor.swift
@@ -14,93 +14,84 @@ final class TodoListInteractor {
 //MARK: - TodoListInteractorInput
 extension TodoListInteractor: TodoListInteractorInput {
     
-    func fetchTodos() {
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            guard let self else { return }
-            if self.isFetching { return }
-            self.isFetching = true
-            
-            let savedTodos = self.coreDataManager.fetchTodos()
-            
-            if savedTodos.isEmpty {
-                self.networkManager.fetchTodos { result in
-                    DispatchQueue.global(qos: .background).async { [weak self] in
-                        guard let self else { return }
-
-                        switch result {
-                        case .success(let todos):
-                            todos.forEach { self.coreDataManager.saveTodo($0) }
-                            self.allTodos = todos
-                            DispatchQueue.main.async {
-                                self.output?.didFetchTodos(todos)
-                            }
-                        case .failure(let error):
-                            DispatchQueue.main.async {
-                                self.output?.didFailWithError(error)
-                            }
-                        }
-                        self.isFetching = false
-                    }
+    func fetchTodos() async {
+        if isFetching { return }
+        isFetching = true
+        defer { isFetching = false }
+        
+        let savedTodos = await Task.detached(priority: .background) { [coreDataManager] in
+            coreDataManager.fetchTodos()
+        }.value
+        
+        if savedTodos.isEmpty {
+            do {
+                let todos = try await networkManager.fetchTodos()
+                
+                await Task.detached(priority: .background) { [coreDataManager] in
+                    todos.forEach { coreDataManager.saveTodo($0) }
+                }.value
+                
+                self.allTodos = todos
+                await MainActor.run {
+                    self.output?.didFetchTodos(todos)
                 }
-            } else {
-                self.allTodos = savedTodos
-                DispatchQueue.main.async {
-                    self.output?.didFetchTodos(savedTodos)
+            } catch {
+                await MainActor.run {
+                    self.output?.didFailWithError(error)
                 }
-                self.isFetching = false
+            }
+        } else {
+            self.allTodos = savedTodos
+            await MainActor.run {
+                self.output?.didFetchTodos(savedTodos)
             }
         }
     }
     
-    func deleteTodo(_ todo: Todo) {
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            guard let self else {return }
-            
-            self.coreDataManager.deleteTodo(todo)
-            self.allTodos.removeAll { $0.id == todo.id }
-            DispatchQueue.main.async {
-                self.output?.didFetchTodos(self.allTodos)
-            }
-        }
-    }
-
-    func toggleTodoCompletion(_ todo: Todo) {
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            guard let self else { return }
-
-            var updatedTodo = todo
-            updatedTodo.isCompleted.toggle()
-            self.coreDataManager.updateTodo(updatedTodo)
-
-            if let index = self.allTodos.firstIndex(where: { $0.id == todo.id }) {
-                self.allTodos[index] = updatedTodo
-            }
-
-            DispatchQueue.main.async {
-                self.output?.didFetchTodos(self.allTodos)
-            }
+    
+    func deleteTodo(_ todo: Todo) async {
+        await Task.detached(priority: .background) { [coreDataManager] in
+            coreDataManager.deleteTodo(todo)
+        }.value
+        
+        self.allTodos.removeAll { $0.id == todo.id }
+        
+        await MainActor.run {
+            self.output?.didFetchTodos(self.allTodos)
         }
     }
     
-    func searchTodos(_ query: String) {
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            guard let self else { return }
-            
-            if query.isEmpty {
-                DispatchQueue.main.async {
-                    self.output?.didFetchTodos(self.allTodos)
-                }
-                return
-            }
+    func toggleTodoCompletion(_ todo: Todo) async {
+        var updatedTodo = todo
+        updatedTodo.isCompleted.toggle()
+        
+        await Task.detached(priority: .background) { [coreDataManager] in
+            coreDataManager.updateTodo(updatedTodo)
+        }.value
+        
+        if let index = self.allTodos.firstIndex(where: { $0.id == todo.id }) {
+            self.allTodos[index] = updatedTodo
+        }
+        
+        await MainActor.run {
+            self.output?.didFetchTodos(self.allTodos)
+        }
+    }
+    
+    func searchTodos(_ query: String) async {
+        let result: [Todo] = await Task.detached(priority: .background) {
+            if query.isEmpty { return self.allTodos }
             
             let loweredQuery = query.lowercased()
-            let filtered = self.allTodos.filter {
+            return self.allTodos.filter {
                 $0.title.lowercased().contains(loweredQuery) ||
                 $0.description.lowercased().contains(loweredQuery)
             }
-            DispatchQueue.main.async {
-                self.output?.didFetchTodos(filtered)
-            }
+        }.value
+        
+        await MainActor.run {
+            self.output?.didFetchTodos(result)
         }
     }
+    
 }
